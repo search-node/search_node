abaa235cf0df21a5864e2a29eba9c089e1b0b19d
Jesper Kristensen <cableman@linuxdev.dk>
2014-11-27 13:58:13 +0100

    Issue IKTREDEL-40 by cableman: Added check on admin API to limit to admin role

--- plugins/admin/admin.js
+++ plugins/admin/admin.js
@@ -21,22 +21,49 @@ var Admin = function Admin(app, logger, Auth) {
    * Default get request.
    */
   app.get('/api/admin', function (req, res) {
-    res.send('Please see documentation about using this administration api.');
+    if (self.validateCall(req)) {
+      res.send('Please see documentation about using this administration api.');
+    }
+    else {
+      res.send('You do not have the right role.', 401);
+    }
   });
 
   /**
    * Get API keys.
    */
   app.get('/api/admin/keys', function (req, res) {
-    res.send('KEYS');
+    if (self.validateCall(req)) {
+      res.send('KEYS');
+    }
+    else {
+      res.send('You do not have the right role.', 401);
+    }
   });
 
   /**
    * Get search indexes.
    */
   app.get('/api/admin/indexes', function (req, res) {
-    res.send('indexes');
+    if (self.validateCall(req)) {
+      res.send('indexes');
+    }
+    else {
+      res.send('You do not have the right role.', 401);
+    }
   });
+};
+
+/**
+ * Validate that the role is admin.
+ *
+ * @param req
+ *   Express request object.
+ */
+Admin.prototype.validateCall = function validateCall(req) {
+  "use strict";
+
+  return (req.hasOwnProperty('user')) && (req.user.role === 'admin');
 };
 
 /**
